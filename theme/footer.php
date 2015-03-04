0be146c7f796df5b9fd1ccc0c87f8bc455554c86
Nicolas C. Galvez <nicholas.galvez@gmail.com>
2015-03-04 09:08:38 -0800

    closes #16, closes #17, and closes #18.

--- theme/footer.php
+++ theme/footer.php
@@ -10,7 +10,18 @@
 
 			</div> <!-- End #site -->
 		<footer id = "colophon">
-			<p>Copyright &copy; <?php echo date('Y')?> <?php bloginfo('name'); ?></p>
+			<div class = "container">
+				<div class = "row">
+					<div id = "copyright" class = "col-xs-12 col-md-3">
+						<p><a href = "<?php bloginfo('url')?>themes/satillite_flight"></a><?php bloginfo('name')?> © <?php echo date('Y')?></p>
+						<p id = "nick-9000" class = "clearfix">
+							<span>Nick</span>
+							<span>Galvez</span>
+						</p> 
+						<p>Urbana, Illinois</p>
+					</div>
+				</div>
+			</div>
 		</footer>
 		<?php wp_footer(); ?>
 	</body>
