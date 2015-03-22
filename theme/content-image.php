72850a263abe9f22d708dff8c3795f5b43c67aff
Nicolas C. Galvez <nicholas.galvez@gmail.com>
2015-03-22 00:57:27 -0700

    Got php tidy to work.

--- theme/content-image.php
+++ theme/content-image.php
@@ -10,39 +10,39 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('format-image'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'format-image' ); ?>>
 	<header class="entry-header">
 		<?php
-			if ( is_single() || is_page() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-			endif;
-		?>
-		
+if ( is_single() || is_page() ) :
+	the_title( '<h1 class="entry-title">', '</h1>' );
+else :
+	the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+endif;
+?>
+
 	</header><!-- .entry-header -->
 	<?php
-	// Post thumbnail.
-		booty_post_thumbnail();
-	?>
+// Post thumbnail.
+booty_post_thumbnail();
+?>
 	<?php if ( is_single() || is_page() ) : ?>
 		<div class="entry-content">
-			<?php	/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s', 'booty' ),
-				the_title( '<span class="screen-reader-text">', '</span>', false )
-			) ); ?>
+			<?php /* translators: %s: Name of current post */
+	the_content( sprintf(
+			__( 'Continue reading %s', 'booty' ),
+			the_title( '<span class="screen-reader-text">', '</span>', false )
+		) ); ?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 		<?php wp_link_pages( array(
-			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'booty' ) . '</span>',
-			'after'       => '</div>',
-			'link_before' => '<span>',
-			'link_after'  => '</span>',
-			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'booty' ) . ' </span>%',
-			'separator'   => '<span class="screen-reader-text">, </span>',
-		) );
-	?>
+		'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'booty' ) . '</span>',
+		'after'       => '</div>',
+		'link_before' => '<span>',
+		'link_after'  => '</span>',
+		'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'booty' ) . ' </span>%',
+		'separator'   => '<span class="screen-reader-text">, </span>',
+	) );
+?>
 
 	<footer class="entry-footer">
 		<?php booty_entry_meta();?>
